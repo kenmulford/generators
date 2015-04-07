b1987ea0f980ba9e4efe85076909eba29003dca6
Ken Mulford <ken@blackpulp.com>
2015-04-06 20:46:57 -0400

    fixed path issue with stub

--- src/Commands/CreateTransformerCommand.php
+++ src/Commands/CreateTransformerCommand.php
@@ -59,7 +59,7 @@ class CreateTransformerCommand extends GeneratorCommand {
    */
   protected function getStub()
   {
-    return __DIR__.'/stubs/transformer.stub';
+    return __DIR__.'/../stubs/transformer.stub';
   }
 
   /**
